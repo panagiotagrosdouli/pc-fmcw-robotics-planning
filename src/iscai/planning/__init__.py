98a091651b6df7ce7b10e09cc9c057a022fbc2bc
panagiotagrosdouli <75089541+panagiotagrosdouli@users.noreply.github.com>
2026-09-05 01:05:08 +0300

    Expose research uncertainty planners

--- src/iscai/planning/__init__.py
+++ src/iscai/planning/__init__.py
@@ -2,6 +2,12 @@ from .dynamics import VehicleParams, rollout, step
 from .trajectory import CandidateTrajectory, generate_candidates
 from .planners import PlanningResult, PredictiveConnectivityPlanner
 from .risk_aware_planner import RiskAwarePredictivePlanner
+from .uncertainty_planners import (
+    AdaptiveConnectivityPlanner,
+    CVaRPredictiveConnectivityPlanner,
+    ChanceConstrainedPredictivePlanner,
+    WorstCasePredictiveConnectivityPlanner,
+)
 
 __all__ = [
     "VehicleParams",
@@ -12,4 +18,8 @@ __all__ = [
     "PlanningResult",
     "PredictiveConnectivityPlanner",
     "RiskAwarePredictivePlanner",
+    "AdaptiveConnectivityPlanner",
+    "CVaRPredictiveConnectivityPlanner",
+    "ChanceConstrainedPredictivePlanner",
+    "WorstCasePredictiveConnectivityPlanner",
 ]
